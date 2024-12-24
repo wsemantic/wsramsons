506a88cd0d227d3e47b413bda3f05cbbea536f8f
Alfonso Gonzalez <info@wsemantic.com>
2024-12-24 19:02:01 +0100

    factura personalizada

--- __manifest__.py
+++ __manifest__.py
@@ -26,7 +26,7 @@
     'data': [
         # 'security/ir.model.access.csv',
         'views/views.xml',
-        'views/templates.xml',
+        'views/invoice.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
