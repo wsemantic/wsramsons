385356f36b20aada372bea4a3b075f4c121c2010
Alfonso Gonzalez <info@wsemantic.com>
2025-03-09 19:48:22 +0100

    Update __manifest__.py

--- __manifest__.py
+++ __manifest__.py
@@ -25,7 +25,7 @@
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
-        'views/views.xml',
+        'views/partner.xml',
         'views/invoice.xml',
         'views/due_list.xml',
     ],
