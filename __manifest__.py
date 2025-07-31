4ba5cb10df9b4d303a997068a4d5ffda57190f64
Alfonso Gonzalez <info@wsemantic.com>
2025-07-31 21:42:24 +0200

    mis contacots

--- __manifest__.py
+++ __manifest__.py
@@ -26,7 +26,7 @@
     'data': [
         'security/groups.xml',
         'security/security_rules.xml',
-        'views/products.xml',
+        'views/product.xml',
         'views/partner.xml',
         'views/invoice.xml',
         'views/due_list.xml',
