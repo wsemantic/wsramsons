7a2909691875f48b0d28611bd81e3f360ce5db1c
Alfonso Gonzalez <info@wsemantic.com>
2025-09-23 19:36:56 +0200

    Add CRM customer menu for commercial group

--- __manifest__.py
+++ __manifest__.py
@@ -20,7 +20,7 @@
     'version': '16.0.0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['sale', 'account'],
+    'depends': ['sale', 'account', 'crm'],
 
     # always loaded
     'data': [
